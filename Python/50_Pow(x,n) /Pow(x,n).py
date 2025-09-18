8540c4ad583ad0490f5ad9508e7fff11d156fd1b
Nikolay <nikolay.p.95.workmail@gmail.com>
2025-09-19 02:02:46 +0400

    Created README rus and eng for Pow(x,n).py

--- Python/50_Pow(x,n) /Pow(x,n).py
+++ Python/50_Pow(x,n) /Pow(x,n).py
@@ -1,6 +1,6 @@
 class Solution:
+    # Через рекурсию
     def myPow(self, x: float, n: int) -> float:
-       # Через рекурсию
         if n == 0:
             return 1
         elif n < 0:
@@ -11,31 +11,35 @@ class Solution:
         else:
             return self.myPow(x * x, n // 2) * x
 
-        # # Через цикл
-        # dop = 1
-        # if n < 0:
-        #     x = 1 / x
-        #     n *= -1
-        # while True:
-        #     if n == 0:
-        #         return 1
-        #     elif n == 1:
-        #         break
-        #
-        #     if n % 2 == 0:
-        #         x = x * x
-        #         n = n // 2
-        #     else:
-        #         dop = x if not dop else dop * x
-        #         x = x * x
-        #         n = n // 2
-        # return x * dop
+    # Через цикл
+    def myPow2(self, x: float, n: int) -> float:
+        dop = 1
+        if n == 0:
+            return 1
+        elif n < 0:
+            x = 1 / x
+            n *= -1
+        while True:
+            if n == 1:
+                break
 
-# test = Solution()
-# print(test.myPow(2.00000, 10))
-# print(test.myPow(2.10000, 3))
-# print(test.myPow(2.00000, -2))
-# print(test.myPow(0.44528, 0))
-# print(test.myPow(8.95371, -1))
+            if n % 2 != 0:
+                dop *= x
+            x *= x
+            n //= 2
+        return x * dop
 
+test = Solution()
+print(test.myPow(2.00000, 10))
+print(test.myPow(2.10000, 3))
+print(test.myPow(2.00000, -2))
+print(test.myPow(0.44528, 0))
+print(test.myPow(8.95371, -1))
+
+
+print(test.myPow2(2.00000, 10))
+print(test.myPow2(2.10000, 3))
+print(test.myPow2(2.00000, -2))
+print(test.myPow2(0.44528, 0))
+print(test.myPow2(8.95371, -1))
 
